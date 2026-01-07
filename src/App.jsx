fa4df015332d0c0cd3a0dcb9c22ddb2c8425bad6
Geovane Silva Prazeres <geovanesil45sil@gmail.com>
2026-01-07 16:19:34 -0300

    Projeto MineBlog em andamento...

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import "./App.css";
 
+//pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+
+//components
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 function App() {
-  return <div className="App">
-    <h1>mineblog</h1>
-  </div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer>
+        <h3>Escreva sobre o que tem interesse</h3>
+        <p>MineBlog &copy; 2026</p>
+    </footer>
+  )
+}
+
+export default Footer
