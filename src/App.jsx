a562680b9d32f3eb71fc292ae4bc29e266403aa1
Geovane Silva Prazeres <geovanesil45sil@gmail.com>
2026-01-09 10:06:06 -0300

    Projeto MineBlog

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import CreatePost from "./pages/CreatePost/CreatePost";
 
 //components
 import NavBar from "./components/NavBar";
@@ -14,23 +16,57 @@ import Footer from "./components/Footer";
 import { AuthProvider } from "./context/AuthContext";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+
+//hooks
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div className="App">
-      <AuthProvider>
+      <AuthProvider value={{ user }}>
         <BrowserRouter>
-        <NavBar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+          <NavBar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/register"
+                element={!user ? <Register /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/posts/create"
+                element={user ? <CreatePost /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/dashboard"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
       </AuthProvider>
     </div>
   );
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,24 @@
 //CSS
-import styles from './About.module.css'
+import styles from "./About.module.css";
+
+//link
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className={styles.about}>
+      <h2>
+        Sobre o Mine <span>Blog</span>
+      </h2>
+      <p>
+        Este projeto consiste em um blog com React no front-end e Firebase no
+        back-end.
+      </p>
+      <Link to="/posts/create" className="btn">
+        Criar Post
+      </Link>
+    </div>
+  );
+};
 
-export default About
+export default About;
